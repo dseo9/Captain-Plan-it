0a59d0c235e88959860d015512f24a9a2276c91b
Mark Tan <mtan53@my.bcit.ca>
2017-05-09 01:18:49 -0700

    made html files into php, created all the other main pages(aboutus.php,avatar.php,contactus.php,footer.php,head.php,index.php,planets.php,stampboard.php), modified footer(changing the help to contact us), added fixed icons for stampboard and information(to be discussed), moved css file inside the css folder, removed downloaded bootstrap files in css, js and fonts folders

---
+++ index.php
@@ -0,0 +1,9 @@
+<!-- loading screen page (1.) center logo, 2.) four corners are the planets, 3.) loading bar (after loading, direct to login screen))-->
+<?php include'head.php'; ?>
+
+
+
+  <div class="row mainbody">
+    <img src="" alt="logo" class="img-responsive center-block" />
+
+  </div>
---
+++ planets.php
@@ -0,0 +1,14 @@
+<?php include 'header.php';?>
+
+
+  <!-- make images responsive and link them to other pages-->
+  <div class="row mainbody">
+    <div class="title_header"><h5>Foodiverse</h5></div>
+
+    <img src="images\fruit_fortress.PNG" alt="Fruit_Fortress" id="fruit_fortress" />
+    <img src="images\pastry_palace.PNG" alt="Pastry_Palace" id="pastry_palace"/>
+    <img src="images\dairy_dome.PNG" alt="Dairy_Dome" id="dairy_dome"/>
+    <img src="images\meat_mansion.PNG" alt="Meat_Mansion" id="meat_mansion"/>
+  </div>
+
+<?php include 'footer.php' ;?>
