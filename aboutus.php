faa85d98537f12cee1c4fc2a8f90c5c84594c622
Mark <mtan53@my.bcit.ca>
2017-05-09 20:59:00 -0700

    fixed about us page, fixed back button, linked logout button to login.php

--- aboutus.php
+++ aboutus.php
@@ -1,10 +1,11 @@
 <?php include 'header.php';?>
 
   <!-- make three row divs for 1.) information about ourgroup, 2.) affiliated apps for kids, 3.) affiliated apps for adults/parents   -->
-
   <div class="row mainbody">
     <div class="title_header"><h5>About Us</h5></div>
+    <br />
 
+    <div class="row subbody">
     <div class="panel panel-info">
       <div class="panel-heading">Group 16</div>
       <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
@@ -14,17 +15,23 @@
       <div class="panel-heading">Affiliates</div>
       <div class="panel-body">
         <div class="panel panel-success">
-          <div class="panel-heading">Food Factory Zero</div>
-          <div class="panel-body">Panel Content</div>
+          <div class="panel-heading">Affiliate Group Number</div>
+          <div class="panel-body">Description</div>
         </div>
         <div class="panel panel-success">
-          <div class="panel-heading">Zero Food Waste</div>
-          <div class="panel-body">Panel Content</div>
+          <div class="panel-heading">Affiliate Group Number</div>
+          <div class="panel-body">Description</div>
         </div>
         <div class="panel panel-success">
-          <div class="panel-heading">Refrigidate</div>
-          <div class="panel-body">Panel Content</div>
+          <div class="panel-heading">Affiliate Group Number</div>
+          <div class="panel-body">Description</div>
         </div>
+        <div class="panel panel-success">
+          <div class="panel-heading">Affiliate Group Number</div>
+          <div class="panel-body">Description</div>
+        </div>
+
+      </div>
       </div>
     </div>
   </div>
