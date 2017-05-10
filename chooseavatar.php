ec8054176ede4bf55949845c9b3ba7682cfb08c9
Mark <mtan53@my.bcit.ca>
2017-05-09 21:30:34 -0700

    creating the choose avatar page

--- chooseavatar.php
+++ chooseavatar.php
@@ -3,6 +3,16 @@
 <div class="row mainbody">
   <div class="title_header"><h5>Are you BOY or GIRL?</h5></div>
 
+    <div class="row-xs-5">
+    <img src="images/boy_avatar.png" alt="girl avatar" class="img-responsive" />
+    </div>
+    <div class="row-xs-2"></div>
+    <div class="row-xs-5">
+      <div class="row-xs-5">
+      <img src="images/girl_avatar.png" alt="girl avatar" class="img-responsive" />
+      </div>
+    </div>
+
 
 </div>
 
