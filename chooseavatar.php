7e0881eb0bf48ffb984a33f8893baaf6ad8f04f0
Mark <mtan53@my.bcit.ca>
2017-05-09 21:45:08 -0700

    working on choose avatar

--- chooseavatar.php
+++ chooseavatar.php
@@ -3,17 +3,17 @@
 <div class="row mainbody">
   <div class="title_header"><h5>Are you BOY or GIRL?</h5></div>
 
+    <div class="row subbody">
     <div class="row-xs-5">
-    <img src="images/boy_avatar.png" alt="girl avatar" class="img-responsive" />
+      <img src="images/boy_avatar.png" alt="boy avatar" class="float-left" />
     </div>
+
     <div class="row-xs-2"></div>
+
     <div class="row-xs-5">
-      <div class="row-xs-5">
-      <img src="images/girl_avatar.png" alt="girl avatar" class="img-responsive" />
-      </div>
+      <img src="images/girl_avatar.png" alt="girl avatar" class="float-right" />
     </div>
-
-
-</div>
+    </div>
+  </div>
 
 <?php include 'footer.php'; ?>
