b7a4ff41c56d24c2ff1b71f427a8d057bcbe675f
Mark <mtan53@my.bcit.ca>
2017-05-11 10:39:49 -0700

    get recent version

--- head.php
+++ head.php
@@ -20,12 +20,9 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<<<<<<< HEAD
-=======
 
     <!-- Google stylesheet for background star image -->
     <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
->>>>>>> develop
 
     <!-- cloudflare core CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -34,9 +31,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.4.0/animate.min.css">
 
     <!-- Custom styles for this template -->
-    <!--<link href="css/main.css" rel="stylesheet" media="screen">
-    <link href="css/main2.css" rel="stylesheet" media="screen">-->
+    <link href="css/main.css" rel="stylesheet" media="screen">
+    <link href="css/main2.css" rel="stylesheet" media="screen">
     <link href="css/main3.css" rel="stylesheet" media="screen">
-
 
   </head>
