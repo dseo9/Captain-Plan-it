99e9dc2075db7f7202562f7bb35e12abce73b2b2
Mark <mtan53@my.bcit.ca>
2017-05-11 16:02:46 -0700

    added avatar page

--- avatar.php
+++ avatar.php
@@ -2,38 +2,79 @@
 
   <!-- make two columns for avatar and customize fields-->
   <div class="row mainbody">
-    <div class="row">
-      <div class="col-sm-3">
-        <img src="images/girl_avatar.png" class="img-responsive center-block"/>
+    <div class="row mainbody">
+      <div class="row subbody">
+        <div class="col-xs-3">
+          <img src="images/girl_avatar.png" class="img-responsive center-block"/>
+        </div>
+
+        <div class="col-xs-9">
+        <div class="row">
+          <div class="col-xs-1"></div>
+          <div class="col-xs-4">
+            <img src="images/dairy_dome.png" class="img-responsive center-block">
+          </div>
+          <div class="col-xs-1"></div>
+          <div class="col-xs-1"></div>
+          <div class="col-xs-4">
+            <button type="button" class="btn btn-primary btn-sm btn-block">SKIN COLOR</button>
+          </div>
+          <div class="col-xs-1"></div>
+        </div>
+
+        <div class="row">
+          <div class="col-xs-1"></div>
+          <div class="col-xs-4">
+            <img src="images/dairy_dome.png" class="img-responsive center-block">
+          </div>
+          <div class="col-xs-1"></div>
+          <div class="col-xs-1"></div>
+          <div class="col-xs-4">
+            <button type="button" class="btn btn-primary btn-sm btn-block">HAIR</button>
+          </div>
+          <div class="col-xs-1"></div>
+        </div>
+
+        <div class="row">
+          <div class="col-xs-1"></div>
+          <div class="col-xs-4">
+            <img src="images/dairy_dome.png" class="img-responsive center-block">
+          </div>
+          <div class="col-xs-1"></div>
+          <div class="col-xs-1"></div>
+          <div class="col-xs-4">
+            <button type="button" class="btn btn-primary btn-sm btn-block">TOP</button>
+          </div>
+          <div class="col-xs-1"></div>
+        </div>
+
+        <div class="row">
+          <div class="col-xs-1"></div>
+          <div class="col-xs-4">
+            <img src="images/dairy_dome.png" class="img-responsive center-block">
+          </div>
+          <div class="col-xs-1"></div>
+          <div class="col-xs-1"></div>
+          <div class="col-xs-4">
+            <button type="button" class="btn btn-primary btn-sm btn-block">BOTTOM</button>
+          </div>
+          <div class="col-xs-1"></div>
+        </div>
+
+        <div class="row">
+          <div class="col-xs-1"></div>
+          <div class="col-xs-4">
+            <img src="images/dairy_dome.png" class="img-responsive center-block">
+          </div>
+          <div class="col-xs-1"></div>
+          <div class="col-xs-1"></div>
+          <div class="col-xs-4">
+            <button type="button" class="btn btn-primary btn-sm btn-block">ACCESSORIES</button>
+          </div>
+          <div class="col-xs-1"></div>
+        </div>
+
       </div>
-
-      <div class="col-sm-9">
-        <div class="row">
-          <div class="col-sm-1"></div>
-          <div class="col-sm-4">
-            <img src="images/dairy_dome.png" class="img-responsive center-block">
-          </div>
-          <div class="col-sm-1"></div>
-          <div class="col-sm-1"></div>
-          <div class="col-sm-4">
-            <button type="button" class="btn btn-primary btn-sm btn-block">Button 1</button>
-          </div>
-          <div class="col-sm-1"></div>
-        </div>
-
-        <div class="row">
-          <div class="col-sm-1"></div>
-          <div class="col-sm-4">
-            <img src="images/dairy_dome.png" class="img-responsive center-block">
-          </div>
-          <div class="col-sm-1"></div>
-          <div class="col-sm-1"></div>
-          <div class="col-sm-4">
-            <button type="button" class="btn btn-primary btn-sm btn-block">Button 1</button>
-          </div>
-          <div class="col-sm-1"></div>
-        </div>
-
       </div>
     </div>
   </div>
