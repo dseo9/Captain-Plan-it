61956aee719424bd37331206289ef85b17e865a9
Jun Park <junhyunpark92@gmail.com>
2017-05-11 16:50:55 -0700

    Changes to dairy.php

--- dairy.php
+++ dairy.php
@@ -11,74 +11,69 @@
       <br>
       <br>
 
-      <div class="row"></div>
+      <div class="row">
+        <div class="col-xs-2">
+        </div>
+        <div class="col-xs-3">
+        </div>
+        <div class="col-xs-2">
+        </div>
+        <div class="col-xs-3">
+        </div>
+        <div class="col-xs-2">
+        </div>
+      </div>
+
+      <br>
 
       <div class="row">
-        <div class="col-sm-3">
-        </div>
-        <div class="col-sm-2">
-        </div>
-        <div class="col-sm-2">
+        <div class="col-xs-2">
           <a href="q1_dairy.php"><img src="images\cow_page\level_1.png" alt="level 1" class="img-responsive center-block"/></a>
         </div>
-        <div class="col-sm-2">
+        <div class="col-xs-3">
         </div>
-        <div class="col-sm-3">
+        <div class="col-xs-2">
         </div>
-      </div>
-
-      <br>
-
-      <div class="row">
-        <div class="col-sm-3">
+        <div class="col-xs-3">
         </div>
-        <div class="col-sm-2">
-          <a href="#"><img src="images\cow_page\key_cow.png" alt="key level" class="img-responsive center-block"/></a>
-        </div>
-        <div class="col-sm-2">
-        </div>
-        <div class="col-sm-2">
-          <a href="#"><img src="images\cow_page\level_2.png" alt="level 2" class="img-responsive center-block"/></a>
-        </div>
-        <div class="col-sm-3">
-        </div>
-      </div>
-
-      <br>
-
-      <div class="row">
-        <div class="col-sm-3">
-        </div>
-        <div class="col-sm-2">
-          <a href="#"><img src="images\cow_page\level_5.png" alt="level 5" class="img-responsive center-block"/></a>
-        </div>
-        <div class="col-sm-2">
-        </div>
-        <div class="col-sm-2">
-          <a href="#"><img src="images\cow_page\level_3.png" alt="level 3" class="img-responsive center-block"/></a>
-        </div>
-        <div class="col-sm-3">
-        </div>
-      </div>
-
-      <br>
-
-      <div class="row">
-        <div class="col-sm-3">
-        </div>
-        <div class="col-sm-2">
-        </div>
-        <div class="col-sm-2">
+        <div class="col-xs-2">
           <a href="#"><img src="images\cow_page\level_4.png" alt="level 4" class="img-responsive center-block"/></a>
         </div>
-        <div class="col-sm-2">
+      </div>
+
+      <br>
+
+      <div class="row">
+        <div class="col-xs-2">
+          <a href="#"><img src="images\cow_page\level_2.png" alt="level 2" class="img-responsive center-block"/></a>
         </div>
-        <div class="col-sm-3">
+        <div class="col-xs-3">
+        </div>
+        <div class="col-xs-2">
+        </div>
+        <div class="col-xs-3">
+        </div>
+        <div class="col-xs-2">
+          <a href="#"><img src="images\cow_page\level_5.png" alt="level 5" class="img-responsive center-block"/></a>
         </div>
       </div>
 
       <br>
 
+      <div class="row">
+        <div class="col-xs-2">
+          <a href="#"><img src="images\cow_page\level_3.png" alt="level 3" class="img-responsive center-block"/></a>
+        </div>
+        <div class="col-xs-3">
+        </div>
+        <div class="col-xs-2">
+        </div>
+        <div class="col-xs-3">
+        </div>
+        <div class="col-xs-2">
+          <a href="#"><img src="images\cow_page\key_cow.png" alt="key level" class="img-responsive center-block"/></a>
+        </div>
+      </div>
     </div>
   </div>
 
