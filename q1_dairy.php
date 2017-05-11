2fc621a7aa9fcd79ef7ffdf8220b9fdcf43a1b43
Jun Park <junhyunpark92@gmail.com>
2017-05-11 10:44:18 -0700

    Fixed dairy and meat question pages, hopefully

--- q1_dairy.php
+++ q1_dairy.php
@@ -1,6 +1,5 @@
 <?php include 'header.php';?>
 
-<<<<<<< HEAD
 <div class="row mainbody">
 	<div class="title_header"><h5>Dairy Dome - Level 1</h5></div>
 	<br/><br/>
@@ -11,46 +10,20 @@
 					Q: Food category question goes here.
 				</div>
 			</div>
-=======
-
-<div class="row mainbody">
-	<div class="title_header"><h5>Foodiverse</h5></div>
-	<br/><br/>
-		<div class="row" id="planets">
-			<div class="col-xs-3"></div>
-<<<<<<< HEAD
-			<div class="col-xs-6">					<div class="questionbox">
-										Q: Food category question goes here.
-								</div>
-							</div>
->>>>>>> develop
-=======
-			<div class="col-xs-6">
-				<div class="questionbox">Q: Food category question goes here.</div>
-			</div>
->>>>>>> mark-feature
 			<div class="col-xs-3"></div>
 		</div>
 
 		<div class="row" id="planets">
 			<div class="col-xs-1"></div>
-<<<<<<< HEAD
 			<div class="col-xs-4"></div>
 			<div class="col-xs-1"></div>
 			<div class="col-xs-1"></div>
 			<div class="col-xs-4"></div>
-=======
-			<div class="col-xs-4">							<input type="button" value="derp 1" class="button" id="q1"></div>
-			<div class="col-xs-1"></div>
-			<div class="col-xs-1"></div>
-			<div class="col-xs-4">							<input type="button" value="derp 2" class="button" id="q1"></div>
->>>>>>> develop
 			<div class="col-xs-1 planets"></div>
 		</div>
 
 		<div class="row" id="planets">
 			<div class="col-xs-1"></div>
-<<<<<<< HEAD
 			<div class="col-xs-4"><input type="button" value="Answer 1" class="btn-group" id="a_dairy"></div>
 			<div class="col-xs-1"></div>
 			<div class="col-xs-1"></div>
@@ -68,33 +41,5 @@
 		</div>
 
 	</div>
-=======
-			<div class="col-xs-4">							<input type="button" value="derp 3" class="button" id="q1"></div>
-			<div class="col-xs-1"></div>
-			<div class="col-xs-1"></div>
-			<div class="col-xs-4">							<input type="button" value="derp 4" class="button" id="q1"></div>
-			<div class="col-xs-1 planets"></div>
-		</div>
-
-
-
-				  <!-- Top portion of the question page -->
-
-					<div class="title">
-						Title of level
-					</div>
-
-
-
-					<!-- Bottom portion of the question page -->
-					<div class="bottom">
-
-
-
-
-					</div>
-
-		  </div>
->>>>>>> develop
 
 <?php include 'footer.php';?>
