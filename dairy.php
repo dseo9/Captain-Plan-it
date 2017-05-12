5ac312c0b61da6bb8104e7b48eccc4f57a5998fc
Jun Park <junhyunpark92@gmail.com>
2017-05-12 00:58:05 -0700

    Changes to dairy.php

--- dairy.php
+++ dairy.php
@@ -5,26 +5,7 @@
   <div class="row mainbody">
     <div class="title_header"><h5>Dairy Dome</h5></div>
 
-    <div class="levels_body dairy_page">
-
-      <br>
-      <br>
-      <br>
-
-      <div class="row">
-        <div class="col-xs-2">
-        </div>
-        <div class="col-xs-3">
-        </div>
-        <div class="col-xs-2">
-        </div>
-        <div class="col-xs-3">
-        </div>
-        <div class="col-xs-2">
-        </div>
-      </div>
-
-      <br>
+    <div class="levels_body dairy_page"><br><br><br><br><br><br><br>
 
       <div class="row">
         <div class="col-xs-2">
@@ -38,6 +19,21 @@
         </div>
         <div class="col-xs-2">
           <a href="#"><img src="images\cow_page\level_4.png" alt="level 4" class="img-responsive center-block"/></a>
+        </div>
+      </div>
+
+      <br>
+
+      <div class="row">
+        <div class="col-xs-2">
+        </div>
+        <div class="col-xs-3">
+        </div>
+        <div class="col-xs-2">
+        </div>
+        <div class="col-xs-3">
+        </div>
+        <div class="col-xs-2">
         </div>
       </div>
 
@@ -62,6 +58,21 @@
 
       <div class="row">
         <div class="col-xs-2">
+        </div>
+        <div class="col-xs-3">
+        </div>
+        <div class="col-xs-2">
+        </div>
+        <div class="col-xs-3">
+        </div>
+        <div class="col-xs-2">
+        </div>
+      </div>
+
+      <br>
+
+      <div class="row">
+        <div class="col-xs-2">
           <a href="#"><img src="images\cow_page\level_3.png" alt="level 3" class="img-responsive center-block"/></a>
         </div>
         <div class="col-xs-3">
@@ -74,6 +85,24 @@
           <a href="#"><img src="images\cow_page\key_cow.png" alt="key level" class="img-responsive center-block"/></a>
         </div>
       </div>
+
+      <br>
+
+      <div class="row">
+        <div class="col-xs-2">
+        </div>
+        <div class="col-xs-3">
+        </div>
+        <div class="col-xs-2">
+        </div>
+        <div class="col-xs-3">
+        </div>
+        <div class="col-xs-2">
+        </div>
+      </div>
+
+      <br><br><br>
+
     </div>
   </div>
 
