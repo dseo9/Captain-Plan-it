af217a0870024a2695c4e563a2ceb7bb4022cb85
Corinne <corinne.kakiuchi@gmail.com>
2017-05-17 15:42:35 -0700

    improve register page, add database sql

--- register.php
+++ register.php
@@ -2,47 +2,47 @@
 
   <div class="row mainbody">
     <div class="title_header"><h5>Who are you Hero?</h5></div>
-    <br/><br/>
+
     <div class="row subbody container-fluid">
       <div class="col-xs-10 col-xs-offset-1">
-      <form action="chooseavatar.php" method="chooseavatar.php" class="">
+      <form action="chooseavatar.php" method="POST" class="">
         <!-- <input type="hidden" name="recipients" class="form-control" id="recipient" value="corinne.kakiuchi@gmail.com">
     		<input type="hidden" name="subject" class="form-control" id="subject" value="Message from Captain-Plan_it webapp">
     		<input type="hidden" name="thanks_url" class="form-control" id="redirection" value="./thanks.php"> -->
 
-        <div class="form-group row">
-          <div class="col-xs-4 col-md-4 img-responsive">
+        <div class="row">
+          <div class="col-sm-6 col-md-2 img-responsive">
             <img src="images/avatar_footer.png"  id="user_icon" alt="User Icon">
           </div>
-          <div class="col-xs-8 col-md-8 has-error input_box">
-            <input type="text" class="form-control" id="red_input" placeholder="Enter Username" name="uname"><span id='uname_error'></span>
+          <div class="col-sm-12 col-md-10 has-error input_box">
+            <input type="text" class="form-control" id="red_input" placeholder="Username" name="uname"><span id='uname_error'></span>
           </div>
         </div>
 
-        <div class="form-group row">
-          <div class="col-xs-4 col-md-4 img-responsive">
+        <div class="row">
+          <div class="col-sm-6 col-md-2 img-responsive">
             <img src="images/register_icons/email_icon.png" id="email_icon" alt="Email Icon">
           </div>
-          <div class="col-xs-8 col-md-8 has-error input_box">
-            <input type="email" class="form-control" id="green_input" placeholder="Enter Email" name="email"><span id='email_error'></span>
+          <div class="col-sm-12 col-md-10 has-error input_box">
+            <input type="email" class="form-control" id="green_input" placeholder="Email" name="email"><span id='email_error'></span>
           </div>
         </div>
 
-        <div class="form-group row">
-          <div class="col-xs-4 col-md-4 img-responsive">
+        <div class="row">
+          <div class="col-sm-6 col-md-2 img-responsive">
             <img src="images/register_icons/password.png" id="password_icon" alt="Password Icon">
           </div>
-          <div class="col-xs-8 col-md-8 has-error input_box">
-            <input type="password" class="form-control" id="purple_input" placeholder="Enter Password" name="psw"><span id='pword_error'></span>
+          <div class="col-sm-12 col-md-10 has-error input_box">
+            <input type="password" class="form-control" id="purple_input" placeholder="Password" name="psw"><span id='pword_error'></span>
           </div>
         </div>
 
-        <div class="form-group row">
-          <div class="col-xs-4 col-md-4 img-responsive">
+        <div class="row">
+          <div class="col-sm-6 col-md-2 img-responsive">
             <img src="images/register_icons/password.png" id="password_icon" alt="Password Icon">
           </div>
-          <div class="col-xs-8 col-md-8 has-error  input_box">
-            <input type="password" class="form-control" id="blue_input" placeholder="Confirm Password" name="psw">
+          <div class="col-sm-12 col-md-10 has-error  input_box">
+            <input type="password" class="form-control" id="blue_input" placeholder="Password" name="psw">
             <span id='cpword_error'></span></br>
           </div>
         </div>
