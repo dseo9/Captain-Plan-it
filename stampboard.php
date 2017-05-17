7f6e847384abda7d5cf5743f3d8fe4224f8c91d6
Mark <mtan53@my.bcit.ca>
2017-05-17 15:55:26 -0700

    deleted duplicate images, sorted out images into folders

--- stampboard.php
+++ stampboard.php
@@ -6,37 +6,37 @@
     <div class="row panel panel-success stampboard_box">
 
     <div class="col-xs-2 col-md-2 img-responsive">
-        <img src="./images/stampboard.png" class="empty img-responsive" data-emptyid="img1" alt="daily stamp">
-        <img src="./images/checked.png" class="checked img-responsive" id="img1" alt="daily stamp">
+        <img src="./images/stampboard/stampboard.png" class="empty img-responsive" data-emptyid="img1" alt="daily stamp">
+        <img src="./images/stampboard/checked.png" class="checked img-responsive" id="img1" alt="daily stamp">
         <p class="h4">DAY 1</p>
     </div>
 
     <div class="col-xs-2 col-md-2 img-responsive">
-        <img src="./images/stampboard.png" class="empty img-responsive" data-emptyid="img2" alt="daily stamp">
-        <img src="./images/checked.png" class="checked img-responsive" id="img2" alt="daily stamp">
+        <img src="./images/stampboard/stampboard.png" class="empty img-responsive" data-emptyid="img2" alt="daily stamp">
+        <img src="./images/stampboard/checked.png" class="checked img-responsive" id="img2" alt="daily stamp">
         <p class="h4">DAY 2</p>
     </div>
 
     <div class="col-xs-2 col-md-2 img-responsive">
-        <img src="./images/stampboard.png" class="empty img-responsive" data-emptyid="img3" alt="daily stamp">
-        <img src="./images/checked.png" class="checked img-responsive" id="img3" alt="daily stamp">
+        <img src="./images/stampboard/stampboard.png" class="empty img-responsive" data-emptyid="img3" alt="daily stamp">
+        <img src="./images/stampboard/checked.png" class="checked img-responsive" id="img3" alt="daily stamp">
         <p class="h4">DAY 3</p>
     </div>
 
     <div class="col-xs-2 col-md-2 img-responsive">
-        <img src="./images/stampboard.png" class="empty img-responsive" data-emptyid="img4" alt="daily stamp">
-        <img src="./images/checked.png" class="checked img-responsive" id="img4" alt="daily stamp">
+        <img src="./images/stampboard/stampboard.png" class="empty img-responsive" data-emptyid="img4" alt="daily stamp">
+        <img src="./images/stampboard/checked.png" class="checked img-responsive" id="img4" alt="daily stamp">
         <p class="h4">DAY 4</p>
     </div>
 
     <div class="col-xs-2 col-md-2  img-responsive">
-        <img src="./images/stampboard.png" class="empty img-responsive" data-emptyid="img5" alt="daily stamp">
-        <img src="./images/checked.png" class="checked img-responsive" id="img5" alt="daily stamp">
+        <img src="./images/stampboard/stampboard.png" class="empty img-responsive" data-emptyid="img5" alt="daily stamp">
+        <img src="./images/stampboard/checked.png" class="checked img-responsive" id="img5" alt="daily stamp">
         <p class="h4">DAY 5</p>
     </div>
 
     <div class="col-xs-2 col-md-2 img-responsive">
-        <img src="./images/stampboard_key.png" class=" img-responsive" alt="daily stamp">
+        <img src="./images/stampboard/stampboard_key.png" class=" img-responsive" alt="daily stamp">
         <p class="h4">KEY DAY!</p>
     </div>
 
