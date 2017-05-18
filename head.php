24e4fa90d71c5b2ec492b69ceb7b40f97134702a
Mark <mtan53@my.bcit.ca>
2017-05-18 13:40:57 -0700

    trying to connect to database

--- head.php
+++ head.php
@@ -1,3 +1,20 @@
+<?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+
+  // Create connection
+  $conn = mysqli_connect($servername, $username, $password);
+
+  // Check connection
+  if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+  }
+
+  SELECT 2 FROM multiplechoice 
+ ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
