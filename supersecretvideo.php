3fbbfefa1efee453bccda2217ac96cf7b6f0ef78
Jun Park <junhyunpark92@gmail.com>
2017-05-23 15:42:24 -0700

    Updated Easter Egg

--- supersecretvideo.php
+++ supersecretvideo.php
@@ -2,11 +2,11 @@
 
   <!-- VIDEO -->
   <div class="row mainbody">
-    <div class="title_header"><h5>Zoinks, we've been made!</h5></div>
+    <div class="title_header"><h5>Earth! Fire! Wind! Water! Heart! GO, PLANET!</h5></div>
     <br /><br />
     <div class="row subbody container-fluid">
 
-	  <iframe style="margin:auto;" width="100%" height="99%" src="https://www.youtube.com/embed/p_xWTBFnzmo?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+	  <iframe style="margin:auto;" width="100%" height="99%" src="https://www.youtube.com/embed/OiYjTb3opAA?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
 
     </div> <!-- ROW SUBBODY END -->
   </div> <!-- ROW MAINBODY END -->
