a69182ed2a2c39b141d96c896e0d70a3cbcd6306
Corinne <corinne.kakiuchi@gmail.com>
2017-05-24 23:00:01 -0700

    changed page when is boy or girl

--- avatar.php
+++ avatar.php
@@ -13,69 +13,46 @@
         <button class="btn btn-warning btn-lg" type="button">Keys <span class="badge">3</span>
         </button>
 
-        <img src="images/girl.png" class="img-responsive center-block avatar_img"/>
+        <!-- If is girl or boy, change the picture -->
+        <img src=
+          <?php if ($_GET['avatar_genre']=='boy') {
+            echo "images/boy.png";
+          } else {
+            echo "images/girl.png";
+          }; ?> class="img-responsive center-block avatar_img"
+        />
         <button type="button" class="btn btn-success btn-lg" name="save">SAVE</button>
       </div>
 
       <div class="col-xs-6">
         <div class="form-group row">
-          <!-- <div class="col-xs-4 col-md-2 col-xs-offset-2">
-            <img src="images/avatar_skin/skin1.png" class="img-responsive center-block">
-          </div> -->
-          <!-- <div class="col-xs-4 col-xs-offset-1"> -->
+
           <a href=""><img src="images/avatar_labels/skin.png" class="img-responsive center-block" alt="skin"></a>
-            <!-- <button type="button" class="btn btn-primary btn-lg btn-block" id="accessory"><p>SKIN</p></button> -->
-          <!-- </div> -->
+
         </div>
 
         <div class="form-group row">
-          <!-- <div class="col-xs-4 col-md-2 col-xs-offset-2">
-            <img src="images/avatar_skin/skin2.png" class="img-responsive center-block">
-          </div> -->
-          <!-- <div class="col-xs-4 col-xs-offset-1"> -->
+
           <a href=""><img src="images/avatar_labels/hair.png" class="img-responsive center-block" alt="skin"></a>
-            <!-- <button type="button" class="btn btn-primary btn-lg btn-block" id="accessory"><p>HAIR</p></button> -->
-          <!-- </div> -->
+
         </div>
 
         <div class="form-group row">
-          <!-- <div class="col-xs-4 col-md-2 col-xs-offset-2">
-            <img src="images/avatar_skin/skin3.png" class="img-responsive center-block">
-          </div> -->
-          <!-- <div class="col-xs-4 col-xs-offset-1"> -->
+
           <a href=""><img src="images/avatar_labels/top.png" class="img-responsive center-block" alt="skin"></a>
-            <!-- <button type="button" class="btn btn-primary btn-lg btn-block" id="accessory"><p>TOP</p></button> -->
-          <!-- </div> -->
+
         </div>
 
         <div class="form-group row">
-          <!-- <div class="col-xs-4 col-md-2 col-xs-offset-2">
-            <img src="images/avatar_skin/skin4.png" class="img-responsive center-block">
-          </div> -->
-          <!-- <div class="col-xs-4 col-xs-offset-1"> -->
+
           <a href=""><img src="images/avatar_labels/buttom.png" class="img-responsive center-block" alt="skin"></a>
-            <!-- <button type="button" class="btn btn-primary btn-lg btn-block" id="accessory"><p>BOTTOM</p></button> -->
-          <!-- </div> -->
+
         </div>
 
         <div class="form-group row">
-          <!-- <div class="col-xs-4 col-md-2 col-xs-offset-2">
-            <img src="images/avatar_skin/skin5.png" class="img-responsive center-block">
-          </div> -->
-          <!-- <div class="col-xs-4 col-xs-offset-1"> -->
+
           <a href=""><img src="images/avatar_labels/access.png" class="img-responsive center-block" alt="skin"></a>
-            <!-- <button type="button" class="btn btn-primary btn-lg btn-block" id="accessory"><p>ACCESSORY</p></button> -->
-          <!-- </div> -->
-        </div>
-		
-		<div class="form-group row">
-		  <div class="col-xs-2 col-md-1 col-xs-offset-2">
-          </div>
-		  <div class="col-xs-2 col-md-1">
-          </div>
-          <div class="col-xs-4 col-xs-offset-1">
-            <button type="button" class="btn btn-success btn-lg btn-block" id="accessory">SAVE</button>
-          </div>
+
         </div>
 
       </div> <!-- COL XS 6 END -->
