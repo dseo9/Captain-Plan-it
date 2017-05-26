e85b8ecec618dfc00c9af1a32329ba521f87f194
Mark <mtan53@my.bcit.ca>
2017-05-25 18:43:22 -0700

    trying to make the header change color when right/wrong is answered

--- questions.php
+++ questions.php
@@ -17,12 +17,12 @@
 				if ($result_ans == "Right answer") {
 					echo "<p>".$result_ans."</p>";
 					$(document).ready(function() {
-						$('instructions').css("background-color","yellow");
+						$('#instructions').css("background-color","yellow");
 					})
 					header("refresh:1; url=fruitplanet.php?p_name=".$_GET['p_name']."&bg=".$_GET['bg']."&page=".$_GET['page'], true, 303);
 				} else {
 					$(document).ready(function() {
-						$('instructions').css("background-color","red");
+						$('#instructions').css("background-color","red");
 					})
 					echo "<p>".$result_ans."</p>";
 				}
